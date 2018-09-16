b394b29f77dabc9397ee7ca408993f991d8a2584
DESKTOP-CQ5JPPO\dirva <dirvareanumarius@live.com>
2018-09-16 19:33:20 +0300

    Map

--- Persoane/Source.cpp
+++ Persoane/Source.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	map<long, string> Persoana;
+	map<long, string>::iterator it;
 	cout << "Hello person" << '\n';
+	Persoana[1] = "Marius";
+	Persoana[2] = "Ionela";
+	for(it=Persoana.begin();it!=Persoana.end();++it)
+	{
+		cout << it->second << endl;
+	}
 	system("PAUSE");
 	return 0;
 }
